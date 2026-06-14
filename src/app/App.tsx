077d76cdf11733ea3d70c126d99c44841e811613
Selva James <149661086+SelvaJames18@users.noreply.github.com>
2026-06-14 16:15:48 +0530

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,6 @@ import { router } from "./routes";
 export default function App() {
   return (
     <div>
-      {/* MARKER-MAKE-KIT-INVOKED */}
       <RouterProvider router={router} />
     </div>
   );
